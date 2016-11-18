74d195efcc2d743dd73fe7c992b8d38e4acb4c2c
Alexgue <cci45@naver.com>
2016-11-18 18:03:40 +0900

    menu

---
+++ memo.js
@@ -0,0 +1,16 @@
+var age
+var name
+var gender
+
+var name = prompt("이름이 어떻게 되시죠?");
+var age = Number(prompt("나이가 어떻게 되시죠?"))
+if ( 20 > age ) {
+  console.log(age);
+} else if ( 40 > age ) {
+  console.log(age);
+} else {
+  console.log(age);
+};
+
+var gender = prompt("성별이 어떻게 되시죠?(남자/여자 중 골라주세요)")
+alert(name + "님," + " 당신이 찾으시는 책이 여기에 있습니다!")
